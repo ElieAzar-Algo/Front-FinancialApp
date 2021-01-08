b9c542b17343bc95bb63145c3b1eddde2834959e
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2021-01-08 04:14:56 +0200

    bootstrap addincome part1

--- src/addIncome/addIncome.js
+++ src/addIncome/addIncome.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Button} from 'react-bootstrap';
 
+
+
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 
 class AddIncome extends React.Component{
@@ -14,13 +23,12 @@ class AddIncome extends React.Component{
         e.preventDefault();
 
         const token = window.localStorage.getItem("token");
-    
+
         const name = e.target.name.value;
         const description = e.target.description.value;
         const amount = e.target.amount.value;
         const date = e.target.date.value;
         const category_id = e.target.category_id.value;
-        //const type_id = e.target.type_id.value;
         const start_date = e.target.start_date.value
         const period = e.target.period.value;
         const repeated= e.target.repeated.value;
@@ -36,21 +44,20 @@ class AddIncome extends React.Component{
                 var newWeek= d.setDate(d.getDate(start_date)+(repeated*7));
                 n = d.toISOString(newWeek);
                 db_date_format=new Intl.DateTimeFormat('fr-ca', { year: 'numeric', month: '2-digit', day: '2-digit'}).format(new Date(Date.parse(n)))
-                //console.log(db_date_format);
+                
             }
             else if(period==="monthly"){
                 
                 var newMonth= d.setMonth(d.getMonth(start_date)+(repeated*1));
                 n = d.toISOString(newMonth);
                 db_date_format=new Intl.DateTimeFormat('fr-ca', { year: 'numeric', month: '2-digit', day: '2-digit'}).format(new Date(Date.parse(n)))
-                //console.log(db_date_format);
+                
             }
             else if(period==="yearly"){
                 var newYear= d.setFullYear(d.getFullYear(start_date)+(repeated*1));
                 n = d.toISOString(newYear);
                 db_date_format=new Intl.DateTimeFormat('fr-ca', { year: 'numeric', month: '2-digit', day: '2-digit'}).format(new Date(Date.parse(n)))
-                console.log(db_date_format);
-
+                
             }
             return db_date_format;
         }
@@ -68,8 +75,8 @@ class AddIncome extends React.Component{
             return type;
         }
         
-        console.log(end_date()); 
-        console.log(type_id())   
+        console.log("End Date --> ",end_date()); 
+        console.log("Type ID --> ",type_id());  
     
     
         const requestOptions = {
@@ -97,6 +104,9 @@ class AddIncome extends React.Component{
         console.log(result);
         
       };
+      //-----------------------------------------------------------------------------------------------------------//
+      //                      the end of create income function and start switch inputs function                   //
+      //-----------------------------------------------------------------------------------------------------------//
 
       Switch = async (e)=>{
         var val=e.target.value;
@@ -122,199 +132,138 @@ class AddIncome extends React.Component{
     render(){
 
 
-        return(
-            <>
-
-
-
-{/* <div className="row row-content">
-                   <div className="col-12">
-                      <h3>Send us your Feedback</h3>
-                   </div>
-                    <div className="col-12 col-md-9">
-                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
-                    <Row className="form-group">
-                                <Label htmlFor="firstname" md={2}>First Name</Label>
-                                <Col md={10}>
-                                    <Control.text model=".firstname" id="firstname" name="firstname"
-                                        placeholder="First Name"
-                                        className="form-control"
-                                        validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }}
-                                         />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".firstname"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                     />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="lastname" md={2}>Last Name</Label>
-                                <Col md={10}>
-                                    <Control.text model=".lastname" id="lastname" name="lastname"
-                                        placeholder="Last Name"
-                                        className="form-control"
-                                        validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }}
-                                         />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".lastname"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be greater than 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                     />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
-                                <Col md={10}>
-                                    <Control.text model=".telnum" id="telnum" name="telnum"
-                                        placeholder="Tel. Number"
-                                        className="form-control"
-                                        validators={{
-                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
-                                        }}
-                                         />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".telnum"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be greater than 2 numbers',
-                                            maxLength: 'Must be 15 numbers or less',
-                                            isNumber: 'Must be a number'
-                                        }}
-                                     />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="email" md={2}>Email</Label>
-                                <Col md={10}>
-                                    <Control.text model=".email" id="email" name="email"
-                                        placeholder="Email"
-                                        className="form-control"
-                                        validators={{
-                                            required, validEmail
-                                        }}
-                                         />
-                                    <Errors
-                                        className="text-danger"
-                                        model=".email"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                            validEmail: 'Invalid Email Address'
-                                        }}
-                                     />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col md={{size: 6, offset: 2}}>
-                                    <div className="form-check">
-                                        <Label check>
-                                            <Control.checkbox model=".agree" name="agree"
-                                                className="form-check-input"
-                                                 /> {' '}
-                                                <strong>May we contact you?</strong>
-                                        </Label>
-                                    </div>
-                                </Col>
-                                <Col md={{size: 3, offset: 1}}>
-                                    <Control.select model=".contactType" name="contactType"
-                                        className="form-control">
-                                        <option>Tel.</option>
-                                        <option>Email</option>
-                                    </Control.select>
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="message" md={2}>Your Feedback</Label>
-                                <Col md={10}>
-                                    <Control.textarea model=".message" id="message" name="message"
-                                        rows="12"
-                                        className="form-control" />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col md={{size:10, offset: 2}}>
-                                    <Button type="submit" color="primary">
-                                    Send Feedback
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                    </div> */}
+        return (
+          <>
             <div className="container">
-                <div className="row row-content">
-                    <form onChange={this.Switch}>
-                    <input type="radio" id="fixed" name="type_id" value="1" defaultChecked />
-                    <label htmlFor="fixed" > Fixed</label>
+              <div className="row row-content">
+                <div className="col-12">
+                  <h3>Add Income</h3>
+                </div>
+              </div>
+              <div className="row row-content">
+                <div className="col-12">
+                  <form onChange={this.Switch}>
+                    <input type="radio"id="fixed" name="type_id" value="1"defaultChecked />
+                    <label htmlFor="fixed"> Fixed</label>
                     <input type="radio" id="recurring" name="type_id" value="2"/>
                     <label htmlFor="recurring">Recurring</label>
+                  </form>
+                </div>
+              </div>
+
+              <div className="row row-content">
+                <div className="col-12">
+                  <div className="container">
+                    <form method="POST" onSubmit={this.createIncome} action="#">
+
+                      <div className="row row-content">
+                        <div className="col-2">
+                          <label htmlFor="name"> Title</label>
+                        </div>
+                        <div>
+                          <input type="text" name="name" id="name" />
+                        </div>
+                      </div>
+
+                      <div className="row row-content">
+                        <div className="col-2">
+                          <label htmlFor="description"> Description</label>
+                        </div>
+                        <div>
+                          <input type="text" name="description" id="description"/>
+                        </div>
+                      </div>
+
+                      <div className="row row-content">
+                        <div className="col-2">
+                          <label htmlFor="amount"> Amount</label>
+                        </div>
+                        <div>
+                          <input type="number" name="amount" id="amount" />
+                        </div>
+                      </div>
+
+                      <div className="row row-content" ref={this.myFixed}>
+                        <div className="col-2">
+                          <label htmlFor="date"> Date</label>
+                        </div>
+                        <div>
+                          <input type="date" name="date" id="date" />
+                        </div>
+                      </div>
+
+                      <div className="row row-content">
+                        <div className="col-2">
+                          <label htmlFor="category_id"> Category</label>
+                        </div>
+                        <div>
+                          <input type="number" name="category_id" id="category"/>
+                        </div>
+                      </div>
+
+                      <div className="row row-content" ref={this.myRecurring}>
+                        <div className="col-4">
+                          <div className="container">
+                            <div className="row">
+                              <div className="col-6">
+                                <label htmlFor="start_date"> Start Date</label>
+                              </div>
+                              <div className="col-4 offset-1">
+                                <input
+                                  type="date"
+                                  name="start_date"
+                                  id="start_date"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="col-4 offset-1">
+                          <div className="container">
+                            <div className="row">
+                              <div className="col-6">
+                                <label htmlFor="period">Choose a period</label>
+                              </div>
+                              <div className="col-6">
+                                <select id="period" name="period">
+                                  <option value="yearly">Yearly</option>
+                                  <option value="monthly">Monthly</option>
+                                  <option value="weekly">Weekly</option>
+                                </select>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="col-2">
+                          <div className="container">
+                            <div className="row">
+                              <div className="col-6">
+                                <label htmlFor="repeated"> Repeated</label>
+                              </div>
+                              <div className="col-6">
+                                <input
+                                  type="number"
+                                  name="repeated"
+                                  id="repeated"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Button color="primary" type="submit" name="submit">
+                        Submit
+                      </Button>
                     </form>
-                    
-                 <form method="POST" onSubmit={this.createIncome}  action="#">
-                <label > Title
-                <input type="text" name="name" id="name" />
-                </label>
-                <label > Description
-                <input type="text" name="description" id="description" />
-                </label>
-                <label > Amount
-                <input type="number" name="amount" id="amount" />
-                </label>
-
-            <div ref={this.myFixed} >
-                <label > date
-                <input type="date" name="date" id="date" />
-                </label>
-            </div>    
-
-
-                <label > Category
-                <input type="number" name="category_id" id="category" />
-                </label>
-
-
-            <div ref={this.myRecurring} >   
-                <label > Start Date
-                <input type="date" name="start_date" id="start_date" />
-                </label>
-                <label htmlFor="period" >Choose a period
-                <select id="period" name="period">
-                <option value="yearly">Yearly</option>
-                <option value="monthly">Monthly</option>
-                <option value="weekly">Weekly</option>
-                </select>
-                </label>
-                <label htmlFor="repeated" >Repeated Time
-                <input type="number" id="repeated" name="repeated"/>
-                </label>
-            </div>    
-
-
-                <input type="submit" name='submit' />
-            </form>
-                   
-        </div>
-  </div>
-              
-               
-            </>
-        )
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
     }
 }
 export default AddIncome;
